5c6e52f9bf38ff8e984ebef2d8a11213be6b9e58
student <behabtuhiwot@gmail.com>
2017-08-08 09:55:37 -0400

    favouriteRecipe

--- src/main/java/hiwotab/favouriterecipeswebsite/controller/MainController.java
+++ src/main/java/hiwotab/favouriterecipeswebsite/controller/MainController.java
@@ -6,10 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
     @GetMapping("/")
+
     public String showHomePage() {
         return "home";
     }
     @GetMapping("/crabcakePage")
+
     public String showCrabcake() {
         return "crabcakes";
     }
